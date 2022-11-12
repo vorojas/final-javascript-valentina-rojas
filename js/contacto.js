da29e0bc24d2181db7a41e866a87379fa82c7056
valentina <vale.rojas058@hotmail.com>
2022-11-11 22:01:10 -0300

    intentando cargar simulador base de datos

--- js/contacto.js
+++ js/contacto.js
@@ -1,25 +1,14 @@
-//const solicitarDatos = () => {
-    //let masestudiantes = 1;
-    //while (masestudiantes !=0) { // condicion que se cumple hasta que el numero ingresado sea cero
-        //let name = prompt('Cual es el nombre del niño/a?');
-        //let sala = prompt('Edad del niño');
-        //let telefono= prompt('Ingrese telefono de contacto');
-        //let horas = prompt('Cuantas horas asistirá el niño al jardín? o ingresa 0 para salir');
-        //masestudiantes = prompt('Vas a ingresar mas estudiantes? 0 para NO');
-        //if (isNaN(horas)) {
-          //  alert("El valor ingresado no es un numero")
-          //  horas = 0;
-        //} else {
-         //   const alumno = new solicitudEstudiante(name, sala, telefono, horas);
-         //   console.log(alumno);
-          //  console.log(alumno.imprimir());
-       // };
-    //};
-
-
-//solicitarDatos();
-
-
+let  archivoAlumnos=  [ ] ;
+    
+    class alumno {
+    //propiedades
+    constructor ( id , nombrePadre , nombreNinio , telefono ) {
+        
+        this (nombrePadre)  = nombrePadre ;
+        this (nombreNinio)  =  nombreNinio ;
+        this (telefono)  =  telefono ;
+    }
+}
 
 //Funcion que calcula el dia de cumpleaños y cuantos dias le faltan para pasar a la proxima sala
 function CumpleañosySala () {
@@ -41,14 +30,19 @@ function calculadorDeDias() {
          if (faltanTantosDias==364){
            document.getElementById("resultado").innerText = "¡Te estas inscribiendo el dia de tu cumpleaños!";
         } else if (faltanTantosDias < 150 && edad < 150){
-           document.getElementById("resultado").innerText = "En ${faltanTantosDias} días pasaras a la proxima sala";
+           document.getElementById("resultado").innerText =`En ${faltanTantosDias} días pasaras a la próxima sala`;
         } else {
            document.getElementById("resultado").innerText = "Ingresa una fecha valida";
         } 
     };
 
 
-    let  archivoAlumnos=  [ ] ;
+    
+
+//simulamos una base de datos creando un arreglo con un objeto alumno
+//entradas.forEach(objeto => { 
+    //archivoAlumnos.push(new alumno(objeto.indice,objeto.nombrePadre,objeto.nombreNinio,objeto.telefono));
+//});
 
 // Funcion para agregar libreria
 
@@ -73,90 +67,40 @@ formularioElement.addEventListener('submit', function(e) {
             agregar();
           } 
 
-        };
-
-    
-
-
-  //carga los registros desde el localstorage si existen. Caso contrario los trae desde la API jsonplaceholder personalizada
-    const obtenerData = async () =>{
-        try {
-            const origen = await axios.get(`https://my-json-server.typicode.com/vorojas/final-javascript-valentina-rojas/entradas`);
-            console.log("Base de datos de ejemplo cargada");
-            origen.data.forEach(objeto => {
-                archivoAlumnos.push(new Impresion(objeto.id,objeto.nombrePadre,objeto.nombreNinio,objeto.telefono));
-            });
-            representaArreglo();
-        } catch (error) {
-            console.log(error);
         }
-    }
+    })
 
-    if (localStorage.getItem('archivoAlumnos') != null){
-        let entradas = [];
-        entradas = JSON.parse(window.localStorage.getItem('archivoAlumnos'));
-        console.log('Base de datos local cargada')
-        entradas.forEach(objeto => {
-            archivoAlumnos.push(new Impresion(objeto.id,objeto.nombrePadre,objeto.nombreNinio,objeto.telefono));
-        });
-    }else{
-        obtenerData();
-    }   
-
-
-    //Agrega un nuevo alumno a la lista y luego actualiza la tabla para mostrarlo
-    if(nombrePadre != "" && nombreNinio != "" && telefono != ""){
-        let alumno = new alumno(generarID(),nombrePadre,nombreNinio,telefono);
-        archivoAlumnos.push(alumno);
-        localStorage.archivoAlumnos = JSON.stringify(archivoAlumnos); //guarda en local storage
-        representaArreglo(archivoAlumnos);
-
-    const mostrarEnLista = (nombrePadre, nombreNinio, telefono) =>{ 
-        let Turno = "";
-    
-       Turnos ? Turno = "Maniana" : Turno = "Tarde";
-    
-        document.getElementById('Tabla1').insertRow(-1).innerHTML = `<tr>
-            <td>${nombrePadre}</td><td>${nombreNinio}</td><td>${telefono}</td>
-            <td>${Turno}</td><tr>`
-            
-    };
-
-    mostrarEnLista ();
-
-    let formularioData = new FormData(formularioElement);
-    for(let pair of formularioData.entries()) {
-        console.log(pair[0]+ ', '+ pair[1]);
-     }
-    };
+    console.log ()
 
 
     //funcion para guardar datos precargados en el localstorage
-    function guardar_localstorage(){
-        let alumno1 = {
-            nombrePadre: "Andrés",
-            nombreNinio: "Mateo",
-            telefonoDeContacto: "351695281",
-        }
-
-        let alumno2 = {
-            nombrePadre: "Romina",
-            nombreNinio: "GianFranco",
-            telefonoDeContacto: "3515403199",
-        }
-
-        let alumno3 = {
-            nombrePadre: "Cristina",
-            nombreNinio: "Catalina",
-            telefonoDeContacto: "3513997509",
-        }
-
-             
-
-    
-        localStorage.setItem ("nombre", JSON.stringify(alumno1, alumno2, alumno3));
-        
+ function guardar_localstorage(){
+    let alumno1 = {
+        nombrePadre: "Andrés",
+        nombreNinio: "Mateo",
+        telefonoDeContacto: "351695281",
     }
-
-
-    guardar_localstorage ()})
+ 
+    let alumno2 = {
+        nombrePadre: "Romina",
+        nombreNinio: "GianFranco",
+        telefonoDeContacto: "3515403199",
+    }
+ 
+    let alumno3 = {
+        nombrePadre: "Cristina",
+        nombreNinio: "Catalina",
+        telefonoDeContacto: "3513997509",
+    }
+ 
+         
+ 
+ 
+    localStorage.setItem ("nombre", JSON.stringify(alumno1,));
+    localStorage.setItem ("nombre", JSON.stringify(alumno2,));
+    localStorage.setItem ("nombre", JSON.stringify(alumno3,));
+   
+}
+ 
+ 
+guardar_localstorage ()
